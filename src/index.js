6a1cb416fa68447de84ba13184a7bdceaa44cfe6
Alexandre Giachetta <alexjacket@outlook.com>
2021-08-01 21:19:02 -0300

    Atividade semana 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './estilo.css'
+import IMG from './Imagens/josedasilva.jpeg'
+
+const ProfSaude = () => {
+    const prof1 = "José da Silva" //Salvou, nesta const, estes três objetos json
+    const prof2 = "Maria da Silva"
+    const prof3 = "Jaqueline Mendes"
+    const estiloexterno = {border: 'solid', borderRadius: 8, backgroundColor: '#EEE', paddingTop: 8, textAlign: 'center', width: '70%'}
+
+    return(
+
+        <div style={estiloexterno}>
+            Profissionais de Saúde
+            <div style={{ border: 'solid', borderRadius: 8, backgroundColor: '#EEE', paddingTop: 8, textAlign: 'center', width: '85%', marginLeft: '7%'}}>
+                <div className="profissionais">
+                    <img src={IMG} style={{display: "inline-grid", border: 'solid', borderRadius: 1, backgroundColor: '#EEE', width: '150px', height: '150px'}} alt='new'></img>
+                    <br/>{prof1}
+                </div>
+                <div className="profissionais">
+                    <img src={process.env.PUBLIC_URL + '/mariadasilva.jpeg'} style={{display: "inline-grid", border: 'solid', borderRadius: 1, backgroundColor: '#EEE', width: '150px', height: '150px'}}  alt='new'/>
+                    <br/>{prof2}</div>
+                <div className="profissionais">
+                    <img src="https://images.pexels.com/photos/4270088/pexels-photo-4270088.jpeg?cs=srgb&dl=pexels-cedric-fauntleroy-4270088.jpg&fm=jpg" style={{display: "inline-grid", border: 'solid', borderRadius: 1, backgroundColor: '#EEE', width: '150px', height: '150px'}} alt='new'/>
+                    <br/>{prof3}</div>
+            </div>
+        </div>
+    )
+}
+//algumas coisas: na linha 21, ele  herdou a imagem de uma pasta imagens que eu cirei auqi.
+//na linha 25, puxei da pasta public
+//na linha 28, puxei de url externa
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <ProfSaude/>,
+    document.querySelector("#root")
+)
